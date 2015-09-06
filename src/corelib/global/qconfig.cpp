4ebe83ad702f6ef31e058a24fa289f33cbf5df9e
Donald Pillou <don.pillou@gmail.com>
2015-09-06 13:23:50 +0200

    Recreated header files 4.8.5

--- src/corelib/global/qconfig.cpp
+++ src/corelib/global/qconfig.cpp
@@ -3,19 +3,19 @@ static const char qt_configure_licensee_str          [512 + 12] = "qt_lcnsuser=O
 static const char qt_configure_licensed_products_str [512 + 12] = "qt_lcnsprod=OpenSource";
 
 /* Build date */
-static const char qt_configure_installation          [11  + 12] = "qt_instdate=2013-12-04";
+static const char qt_configure_installation          [11  + 12] = "qt_instdate=2015-09-06";
 
-static const char qt_configure_prefix_path_str       [512 + 12] = "qt_prfxpath=D:\\git\\libcppqt\\qt";
-static const char qt_configure_documentation_path_str[512 + 12] = "qt_docspath=D:\\git\\libcppqt\\qt\\doc";
-static const char qt_configure_headers_path_str      [512 + 12] = "qt_hdrspath=D:\\git\\libcppqt\\qt\\include";
-static const char qt_configure_libraries_path_str    [512 + 12] = "qt_libspath=D:\\git\\libcppqt\\qt\\lib";
-static const char qt_configure_binaries_path_str     [512 + 12] = "qt_binspath=D:\\git\\libcppqt\\qt\\bin";
-static const char qt_configure_plugins_path_str      [512 + 12] = "qt_plugpath=D:\\git\\libcppqt\\qt\\plugins";
-static const char qt_configure_imports_path_str      [512 + 12] = "qt_impspath=D:\\git\\libcppqt\\qt\\imports";
-static const char qt_configure_data_path_str         [512 + 12] = "qt_datapath=D:\\git\\libcppqt\\qt";
-static const char qt_configure_translations_path_str [512 + 12] = "qt_trnspath=D:\\git\\libcppqt\\qt\\translations";
-static const char qt_configure_examples_path_str     [512 + 12] = "qt_xmplpath=D:\\git\\libcppqt\\qt\\examples";
-static const char qt_configure_demos_path_str        [512 + 12] = "qt_demopath=D:\\git\\libcppqt\\qt\\demos";
+static const char qt_configure_prefix_path_str       [512 + 12] = "qt_prfxpath=D:\\git\\HelloQt\\Ext\\qt";
+static const char qt_configure_documentation_path_str[512 + 12] = "qt_docspath=D:\\git\\HelloQt\\Ext\\qt\\doc";
+static const char qt_configure_headers_path_str      [512 + 12] = "qt_hdrspath=D:\\git\\HelloQt\\Ext\\qt\\include";
+static const char qt_configure_libraries_path_str    [512 + 12] = "qt_libspath=D:\\git\\HelloQt\\Ext\\qt\\lib";
+static const char qt_configure_binaries_path_str     [512 + 12] = "qt_binspath=D:\\git\\HelloQt\\Ext\\qt\\bin";
+static const char qt_configure_plugins_path_str      [512 + 12] = "qt_plugpath=D:\\git\\HelloQt\\Ext\\qt\\plugins";
+static const char qt_configure_imports_path_str      [512 + 12] = "qt_impspath=D:\\git\\HelloQt\\Ext\\qt\\imports";
+static const char qt_configure_data_path_str         [512 + 12] = "qt_datapath=D:\\git\\HelloQt\\Ext\\qt";
+static const char qt_configure_translations_path_str [512 + 12] = "qt_trnspath=D:\\git\\HelloQt\\Ext\\qt\\translations";
+static const char qt_configure_examples_path_str     [512 + 12] = "qt_xmplpath=D:\\git\\HelloQt\\Ext\\qt\\examples";
+static const char qt_configure_demos_path_str        [512 + 12] = "qt_demopath=D:\\git\\HelloQt\\Ext\\qt\\demos";
 /* strlen( "qt_lcnsxxxx") == 12 */
 #define QT_CONFIGURE_LICENSEE qt_configure_licensee_str + 12;
 #define QT_CONFIGURE_LICENSED_PRODUCTS qt_configure_licensed_products_str + 12;
